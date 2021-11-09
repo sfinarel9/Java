0f31a17cf33da89e30abceab710e78cacde06463
Sfinarolaki Eleftheria <60104748+sfinarel9@users.noreply.github.com>
2021-11-09 19:41:19 +0200

    Add files via upload

---
+++ Matching Game/src/projectsemester2019/ProjectSemester2019.java
@@ -0,0 +1,10 @@
+package projectsemester2019;
+
+public class ProjectSemester2019 {
+
+    public static void main(String[] args) {
+        StartScreen ss = new StartScreen();
+        ss.start_screen.setVisible(true);
+    }
+
+}
